2bae75f74d0b87a9cea6402b2df421f044a3638a
LeandroPS <leandro.pires.souza@gmail.com>
2022-09-10 13:38:45 -0300

    feat(Vote Page): Add username to interface

--- src/pages/Vote.js
+++ src/pages/Vote.js
@@ -2,9 +2,11 @@ import TeamMemberList from "../components/TeamMemberList";
 import TeamVotes from "../components/TeamVotes";
 import VoteSelector from "../components/VoteSelector";
 import { usePlanning } from "../context/planning";
+import { useSettings } from "../context/settings";
 
 const Vote = () => {
   const { state, vote, revealVotes, clearVotes } = usePlanning();
+  const { settings } = useSettings();
   const { team, votes, showVotes } = state;
 
   const handleVote = (value) => {
@@ -13,10 +15,14 @@ const Vote = () => {
 
   return (
     <>
+      <h2>{settings.name}</h2>
+      <h3>Team:</h3>
       <TeamMemberList teamMembers={team} />
+      <h3>Votes:</h3>
       <TeamVotes votes={Object.values(votes)} revealed={showVotes} />
       <button onClick={revealVotes}>Reveal votes</button>
       <button onClick={clearVotes}>Clear votes</button>
+      <h3>Votation options:</h3>
       <VoteSelector onSelect={handleVote} />
     </>
   );
