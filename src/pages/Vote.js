c1895d6668e5e37402081b140ac29b08810f40ab
LeandroPS <leandro.pires.souza@gmail.com>
2022-09-14 11:10:36 -0300

    feat(VotePage): Add leave button

--- src/pages/Vote.js
+++ src/pages/Vote.js
@@ -1,12 +1,14 @@
 import TeamMemberList from "../components/TeamMemberList";
 import TeamVotes from "../components/TeamVotes";
 import VoteSelector from "../components/VoteSelector";
+import { useNavigate } from "react-router-dom";
 import { usePlanning } from "../context/planning";
 import { useSettings } from "../context/settings";
 
 const Vote = () => {
-  const { state, vote, revealVotes, clearVotes, setClearVotesHandler } =
+  const { state, vote, revealVotes, clearVotes, leave, setClearVotesHandler } =
     usePlanning();
+  const navigate = useNavigate();
   const { settings } = useSettings();
   const { team, votes, showVotes } = state;
 
@@ -14,16 +16,22 @@ const Vote = () => {
     vote({ value });
   };
 
+  const handleLeave = () => {
+    leave();
+    navigate("/");
+  };
+
   return (
     <>
       <h2>{settings.name}</h2>
+      <button onClick={handleLeave}>Leave</button>
       <h3>Team:</h3>
       <TeamMemberList teamMembers={team} />
       <h3>Votes:</h3>
       <TeamVotes votes={Object.values(votes)} revealed={showVotes} />
       <button onClick={revealVotes}>Reveal votes</button>
       <button onClick={clearVotes}>Clear votes</button>
-      <h3>Votation options:</h3>
+      <h3>Vote options:</h3>
       <VoteSelector
         onSelect={handleVote}
         setClearVotesHandler={setClearVotesHandler}
